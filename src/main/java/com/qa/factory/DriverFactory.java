071781e16e195fd91664c427e1a7279a133452a1
anilbasudkar <anil.basudkar@gmail.com>
2023-09-21 12:54:16 -0400

    my changes

--- src/main/java/com/qa/factory/DriverFactory.java
+++ src/main/java/com/qa/factory/DriverFactory.java
@@ -1,48 +1,36 @@
 package com.qa.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.Collections;
 
 public class DriverFactory {
-	public WebDriver driver;
-	
-	public static ThreadLocal<WebDriver> tlDriver= new ThreadLocal<WebDriver>();
-	
-	public WebDriver init_driver(String Browser) {
-		System.out.println("browser value is :" +Browser);
-		
-		if(Browser.equals("Chrome")) {
-			
-			WebDriverManager.chromedriver().setup();
-			tlDriver.set(new ChromeDriver());
-		   }
-		else if(Browser.equals("firefox")) {
+    public WebDriver driver;
 
-			WebDriverManager.firefoxdriver().setup();
-			tlDriver.set(new FirefoxDriver());
-		   }
-		else if(Browser.equals("safari")) {
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 
-			WebDriverManager.safaridriver().setup();
-			tlDriver.set(new SafariDriver());
-			}
-		else {
-			System.out.println("Please pass the correct browser value :" + Browser);
-		}
-		return getDriver();
-		
-	}
-	public static synchronized WebDriver getDriver() {
-		return tlDriver.get();
-		
-	}
-	
-	
-	
-	
+    public WebDriver init_driver(String Browser) {
+        System.out.println("browser value is :" + Browser);
+        if (Browser.equals("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            tlDriver.set(new ChromeDriver());
+        } else if (Browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            tlDriver.set(new FirefoxDriver());
+        } else if (Browser.equals("safari")) {
+            WebDriverManager.safaridriver().setup();
+            tlDriver.set(new SafariDriver());
+        } else {
+            System.out.println("Please pass the correct browser value :" + Browser);
+        }
+        return getDriver();
+    }
+
+    public static synchronized WebDriver getDriver() {
+        return tlDriver.get();
+    }
+
+
 }
