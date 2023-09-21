f76a4f1f8d6bf697281a5d0d3caa90813e19b840
anilbasudkar <anil.basudkar@gmail.com>
2023-09-21 13:29:29 -0400

    my changes

--- src/test/java/hooks/ApplicationHooks.java
+++ src/test/java/hooks/ApplicationHooks.java
@@ -15,45 +15,36 @@ import io.cucumber.java.Scenario;
 
 public class ApplicationHooks {
 
-	private DriverFactory driverfactory;
-	private WebDriver webDriver;
-	private ConfigReader configReader;
-	Properties prop;
-	
-	@Before(order = 0)
-	public void getProperty() {
-		
-		configReader=new ConfigReader();
-		prop = configReader.init_prop();
-		
-		
-	}
-	
-	@Before(order = 1)
-	public void launchBrowser() {
-		String browserName = prop.getProperty("browser");
-		driverfactory = new DriverFactory();
-		webDriver =driverfactory.init_driver(browserName);
-		
-		
-	}
-	
-	@After(order = 0)
-	public void browserQuit() {
-		webDriver.close(); 
-	}
-	
-	@After(order = 1)
-	public void tearDown(Scenario scenario) {
-		if (scenario.isFailed()) {
-			//take screenshot
-			String screenShotName = scenario.getName().replaceAll(" ", "_");
-			byte[] sourcePath=((TakesScreenshot)webDriver).getScreenshotAs(OutputType.BYTES);
-			scenario.attach(sourcePath, "image/png", screenShotName);
-		}
-			
-		
-	}
-	
-	
+    private DriverFactory driverfactory;
+    private WebDriver webDriver;
+    private ConfigReader configReader;
+    Properties prop;
+
+    @Before(order = 0)
+    public void getProperty() {
+        configReader = new ConfigReader();
+        prop = configReader.init_prop();
+    }
+
+    @Before(order = 1)
+    public void launchBrowser() {
+        String browserName = prop.getProperty("browser");
+        driverfactory = new DriverFactory();
+        webDriver = driverfactory.init_driver(browserName);
+    }
+
+    @After(order = 0)
+    public void browserQuit() {
+        webDriver.close();
+    }
+
+    @After(order = 1)
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            //take screenshot
+            String screenShotName = scenario.getName().replaceAll(" ", "_");
+            byte[] sourcePath = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(sourcePath, "image/png", screenShotName);
+        }
+    }
 }
